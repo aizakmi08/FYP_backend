d5b7d74dd58878f3d4b4eafb23629e29d616e0a2
Ulugbek Karimov <ulugbek.karimov_2024@ucentralasia.org>
2024-04-24 18:39:54 +0600

    adding booking system

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,24 @@
+"""URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
-
-from user_authentication.views import UserActivationView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('auth/', include('rest_framework.urls')),
-    path('auth/', include('djoser.urls')),
-    path('user_authentication/auth/', include('djoser.urls')),
-    path('user_authentication/auth/', include('djoser.urls.authtoken')),
-    path('user_authentication/auth/', include('djoser.urls.jwt')),
-    path('activate/<str:uid>/<str:token>/', UserActivationView.as_view(), name='user_activation'),
-
-    path('booking_system/', include('booking_system.urls')),
-    path('report/', include('report.urls')),
-
-]
-
-# urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+    path('', include('reservationApp.urls')),
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
