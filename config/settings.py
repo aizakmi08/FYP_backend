5b6ea6137c82206e0ed1c251730b0bb9a759f724
Ulugbek Karimov <ulugbek.karimov_2024@ucentralasia.org>
2024-05-15 18:18:25 +0600

    statics updated

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # STATIC_URL = 'static_files/'
-# STATIC_ROOT = path.join(BASE_DIR, 'static_files/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # # Static files (CSS, JavaScript, Images)
 # # https://docs.djangoproject.com/en/4.0/howto/static-files/
